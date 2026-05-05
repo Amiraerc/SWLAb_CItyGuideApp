0e3ab72e7b5b1e6c7b905c15785de690634b25b1
Amiraerc <amiraamrenova@gmail.com>
2026-05-05 04:55:15 +0200

    Add files via upload

---
+++ SWLab/LAB6/game.py
@@ -0,0 +1,31 @@
+class Game:
+    def __init__(self):
+        self.rolls = [0] * 21
+        self.current_roll = 0
+    
+    def roll(self, pins):
+        self.rolls[self.current_roll] = pins
+        self.current_roll += 1
+    
+    def score(self):
+        score = 0
+        frame_index = 0
+        
+        for frame in range(10):
+            if self._is_strike(frame_index):
+                score += 10 + self.rolls[frame_index + 1] + self.rolls[frame_index + 2]
+                frame_index += 1
+            elif self._is_spare(frame_index):
+                score += 10 + self.rolls[frame_index + 2]
+                frame_index += 2
+            else:
+                score += self.rolls[frame_index] + self.rolls[frame_index + 1]
+                frame_index += 2
+        
+        return score
+    
+    def _is_strike(self, frame_index):
+        return self.rolls[frame_index] == 10
+    
+    def _is_spare(self, frame_index):
+        return self.rolls[frame_index] + self.rolls[frame_index + 1] == 10
---
+++ SWLab/LAB6/test_game.py
@@ -0,0 +1,47 @@
+import unittest
+from game import Game
+
+class TestBowlingGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.g = Game()
+    
+    def roll_many(self, n, pins):
+        for i in range(n):
+            self.g.roll(pins)
+    
+    def roll_strike(self):
+        self.g.roll(10)
+    
+    def roll_spare(self):
+        self.g.roll(5)
+        self.g.roll(5)
+    
+    def test_gutter_game(self):
+        self.roll_many(20, 0)
+        self.assertEqual(0, self.g.score())
+    
+    def test_all_ones(self):
+        self.roll_many(20, 1)
+        self.assertEqual(20, self.g.score())
+    
+    def test_one_spare(self):
+        self.roll_spare()
+        self.g.roll(3)
+        self.roll_many(17, 0)
+        self.assertEqual(16, self.g.score())
+    
+    def test_one_strike(self):
+        self.roll_strike()
+        self.g.roll(3)
+        self.g.roll(4)
+        self.roll_many(16, 0)
+        self.assertEqual(24, self.g.score())
+    
+    def test_perfect_game(self):
+        for i in range(12):
+            self.roll_strike()
+        self.assertEqual(300, self.g.score())
+
+if __name__ == '__main__':
+    unittest.main()
